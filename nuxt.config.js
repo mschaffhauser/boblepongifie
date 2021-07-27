b8a27c7e39d93e782546d7635b780139287dc34e
MorNight <maximeschafo@gmail.com>
2021-07-28 00:53:44 +0200

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   router: {
-  base: '/bob/'
+    base: '/bob/'
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
